fab6c31e668624a651a7fe095c74c2ad9ac1a9ee
gabriel.daniel <gabriel.daniel@sou.ufac.br>
2024-08-05 10:16:12 -0500

    Revert "Revert "fiz no lab - 05/08/24""
    This reverts commit f8e7b2b04c0590de0f4c503ba261f560f3f180e4.

---
+++ 3104 - Resto 2.0.py
@@ -0,0 +1,10 @@
+import sys
+sys.set_int_max_str_digits(9999999) #vei deu tudo errado agr entendi pq q eh uma das mais dificeis da porra do bee
+
+a = int(input())
+b = int(input())
+print(a % b)
+
+
+
+#DEU RUNTIME ERROR ESSA PORRA AF MERDA prikito
