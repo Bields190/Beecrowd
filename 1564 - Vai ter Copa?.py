f8e7b2b04c0590de0f4c503ba261f560f3f180e4
gabriel.daniel <gabriel.daniel@sou.ufac.br>
2024-08-05 10:15:44 -0500

    Revert "fiz no lab - 05/08/24"
    This reverts commit 8c969f1a472df3f03b90a27ff1b51e2c3496ef84.

--- 1564 - Vai ter Copa?.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        n = int(input())
-        if n > 0:
-            print("vai ter duas!")
-        elif n == 0:
-            print("vai ter copa!")
-    except EOFError:
-        break
